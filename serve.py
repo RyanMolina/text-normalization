453c72b83592310389b1b478246326275740ef9b
RyanMolina <ryangabrielpmolina@gmail.com>
2018-01-18 00:29:36 +0800

    comply to flake8

--- serve.py
+++ serve.py
@@ -3,13 +3,16 @@ import os
 from nltk.tokenize import word_tokenize, sent_tokenize
 import seq2seq
 
+
 class Serve:
     """Serve an instance of the trained model"""
     def __init__(self, sess, model_name, checkpoint):
-        os.makedirs(os.path.join('training', 'data', 'dataset', model_name), exist_ok=True)
+        os.makedirs(os.path.join('training', 'data', 'dataset', model_name),
+                    exist_ok=True)
         data_dir = os.path.join('training', 'data', 'dataset', model_name)
         model_dir = os.path.join('training', 'model', model_name)
-        hparams = seq2seq.utils.load_hparams(os.path.join(model_dir, 'char_level_hparams.json'))
+        hparams = seq2seq.utils.load_hparams(
+            os.path.join(model_dir, 'char_level_hparams.json'))
         self.normalizer = seq2seq.predictor.Predictor(sess,
                                                       dataset_dir=data_dir,
                                                       output_dir=model_dir,
